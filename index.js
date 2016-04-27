cadae70dcf808ef48bcf5dbeeab99571773f22bf
Lsong <song940@gmail.com>
2016-04-27 16:29:48 +0800

    modify .

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 'use strict';
+/**
+ * [exports Kelp]
+ * @return {[type]} [description]
+ */
 module.exports = function Kelp(){
   /**
-   * [app description]
-   * @param  {[type]} req [description]
-   * @param  {[type]} res [description]
-   * @return {[type]}     [description]
+   * [app http server handler]
+   * @param  {[type]} req [request]
+   * @param  {[type]} res [response]
+   * @return {[type]}     [undefined]
    */
   function app(req, res){
     var self = this, i = -1;
     (function next(err){
       var middleware = app.stack[ ++i ];
       if(typeof middleware == 'function'){
-        middleware.apply(self, [ req, res, next, err ]);
+        middleware.apply(self, [ req, res, next ]);
       }
     })();
   };
   /**
-   * [stack description]
+   * [stack middlewares]
    * @type {Array}
    */
   app.stack  = [];
   /**
-   * [function description]
-   * @param  {[type]} middlewares [description]
-   * @return {[type]}             [description]
+   * [function push middleware to stack]
+   * @param  {[type]} middlewares [middlewares]
+   * @return {[type]}             [application]
    */
   app.use = function(middlewares){
     this.stack = [].concat.apply(this.stack, arguments);
