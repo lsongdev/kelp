8a3f0463b8b1a959a943e36beb751dd4714ed752
Lsong <song940@gmail.com>
2018-06-29 11:48:19 +0800

    2.x

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
  * [exports Kelp]
  * @return {[type]} [description]
  */
-module.exports = function Kelp(){
+module.exports = function Kelp() {
   /**
    * [app http server handler]
    * @param  {[type]} req [request]
    * @param  {[type]} res [response]
    * @return {[type]}     [undefined]
    */
-  function app(req, res){
+  function app(req, res) {
     var self = this, i = -1;
-    (function next(err){
-      var middleware = app.stack[ ++i ];
-      if(typeof middleware == 'function'){
-        middleware.apply(self, [ req, res, next ]);
+    (async function next(err) {
+      var middleware = app.stack[++i];
+      if (typeof middleware == 'function') {
+        await middleware.apply(self, [req, res, next]);
       }
     })();
   };
@@ -23,13 +23,13 @@ module.exports = function Kelp(){
    * [stack middlewares]
    * @type {Array}
    */
-  app.stack  = [];
+  app.stack = [];
   /**
    * [function push middleware to stack]
    * @param  {[type]} middlewares [middlewares]
    * @return {[type]}             [application]
    */
-  app.use = function(middlewares){
+  app.use = function (middlewares) {
     this.stack = [].concat.apply(this.stack, arguments);
     return app;
   };
